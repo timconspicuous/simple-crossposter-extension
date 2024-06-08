0265393edb358d75bc993fb36e5c329cd587c1f5
timconspicuous <bipolarfuchs@gmail.com>
2024-06-08 18:27:23 +0200

    Added rudimentary image support

--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,4 @@
-async function postToBluesky(text) {
+async function postToBluesky(text, imageURL = null, timestamp = new Date().toISOString()) {
     const { BskyAgent } = await import("@atproto/api");
 
     // Function to retrieve credentials as a promise
@@ -13,33 +13,105 @@ async function postToBluesky(text) {
         });
     }
 
-    function composePost(text, image = null, timestamp = null) {
-        const postJSON = {};
+    /*function getImageDimensions(url) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => {
+                resolve({ width: img.width, height: img.height });
+            };
+            img.onerror = (error) => {
+                reject(error);
+            };
+            img.src = url;
+        });
+    }*/
+
+    /*function toBlob(canvas, type = "image/jpeg", quality = 1) {
+        return new Promise((resolve) => canvas.toBlob(blob => resolve(blob)))
+    }*/
+      
+    async function imageToUint8Array(image, context) {
+        context.canvas.width = image.width;
+        context.canvas.height = image.height;
+        context.drawImage(image, 0, 0);
         
+        const blob = await new Promise((resolve) => {
+            context.canvas.toBlob(
+                (blob) => {
+                    resolve(blob);
+                },
+                "image/jpeg",
+                1
+            );
+        });
+        return new Uint8Array(await blob.arrayBuffer());
     }
 
+    /*function imageToUint8Array(image, context) {
+        context.width = image.width;
+        context.height = image.height;
+        context.drawImage(image, 0, 0);
+
+        return new Uint8Array(context.getImageData(0, 0, image.width, image.height).data.buffer);
+    }*/
+
+    const agent = new BskyAgent({
+        service: "https://bsky.social"
+    });
+
+    let username;
+    let password;
     try {
-        const { username, password } = await getCredentials();
-
-        const agent = new BskyAgent({
-            service: "https://bsky.social"
-        });
-
-        if (username && password) {
-            await agent.login({
-                identifier: username,
-                password: password
-            });
-            await agent.post({
-                text: "[🤖🔁]: " + text,
-                createdAt: new Date().toISOString()
-            });
-            console.log("Post successful!");
-        } else {
-            console.log("No credentials stored");
-        }
+        const credentials = await getCredentials();
+        username = credentials.username;
+        password = credentials.password;
     } catch (error) {
-        console.error("Error retrieving credentials or posting:", error);
+        console.error("Error retrieving credentials: ", error);
+    }
+        
+    const postJSON = {};
+    postJSON.text = text;
+    postJSON.createdAt = timestamp;
+
+    if (username && password) {
+        await agent.login({
+            identifier: username,
+            password: password
+        });
+    }
+
+    if (imageURL) {
+        const tempImage = new Image();
+        tempImage.src = imageURL;
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+
+        postJSON.embed = {$type: "app.bsky.embed.images"};
+        postJSON.embed.images = [{}];
+        /*getImageDimensions(imageURL)
+            .then(dimensions => {
+                postJSON.embed.images[0].aspectRatio = dimensions;
+            })
+            .catch(error => {
+                console.error("Error loading image:", error);
+            });*/
+        postJSON.embed.images[0].aspectRatio = {width: tempImage.width, height: tempImage.height};
+
+        try {
+            const { data } = await agent.uploadBlob(await imageToUint8Array(tempImage, context), {encoding: "image/jpeg"}); //include some encoding if necessary
+            postJSON.embed.images[0].image = data.blob;
+        } catch(error) {
+            console.error("Error uploading file: ", error);
+        }
+        postJSON.embed.images[0].alt = "";
+    }
+
+    console.log(postJSON);
+
+    try {
+        await agent.post(postJSON);
+    } catch(error) {
+        console.error("Error submitting post: ", error);
     }
 }
 
@@ -91,10 +163,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     document.getElementById("postToBluesky").addEventListener("click", async () => {
         try {
-            postToBluesky(document.getElementById("text").innerText);
+            if (document.getElementById("image").src) {
+                postToBluesky(document.getElementById("text").innerText, document.getElementById("image").src); // This should be an array of images
+            } else {
+                postToBluesky(document.getElementById("text").innerText);
+            }
         } catch (error) {
             console.error(error);
-            //document.getElementById('status').innerText = 'Post failed.';
         }
     });
 });
